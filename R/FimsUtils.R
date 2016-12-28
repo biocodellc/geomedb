b661230bcbda2d7da33eb5b343d4348dc64e2bb7
rjewing <ewing.rj@gmail.com>
2016-12-28 09:17:04 -0800

    Can now fetch fasta sequences in DNABin format
    also use devtools::document() to create NAMESPACE file and documentation

--- R/FimsUtils.R
+++ R/FimsUtils.R
@@ -9,14 +9,16 @@ library(httr)
 # CONSTANTS
 projectId <- 25
 
-fimsRestRoot <- "http://biscicol.org/dipnet/rest"
-#fimsRestRoot <- "http://localhost:8080/dipnet/rest"
+#fimsRestRoot <- "http://biscicol.org/dipnet/rest"
+fimsRestRoot <- "http://localhost:8080/dipnet/rest"
 fimsLoginServiceUrl  <- paste(fimsRestRoot, "authenticationService", "login", sep="/")
 fimsProjectExpeditionsUrl <- paste(fimsRestRoot, "projects", projectId, "expeditions", sep="/")
 fimsQueryUrl <- paste(fimsRestRoot, "projects", "query", "csv", sep="/")
+fimsFastaQueryUrl <- paste(fimsRestRoot, "projects", "query", "fasta", sep="/")
 
-# Must call this first!
-# authenticate, necessary to fetch data which is private
+#' Must call this first!
+#' authenticate, necessary to fetch data which is private
+#' @export
 authenticate <- function(user, pass) {
 
     r <- POST(fimsLoginServiceUrl,
@@ -30,7 +32,8 @@ authenticate <- function(user, pass) {
     stop_for_status(r)
 }
 
-# get a list of expeditions to query against
+#' get a list of expeditions to query against
+#' @export
 listExpeditions <- function() {
 
     r <- GET(fimsProjectExpeditionsUrl)
@@ -51,26 +54,9 @@ listExpeditions <- function() {
 #' @param names       list of column names to include in the data.frame results
 #' @param filters     list of "column name":"value" pairs to include in the query. The special column name "_all" will do
 #'                    a full text search in all columns for the value
-query <- function(expeditions=list(), filters=list(), names=NULL) {
-    query.params = ""
-
-    if (length(expeditions) > 0) {
-        expedition.names <- rep("expeditions", length(expeditions))
-        # for each expeditionCode in list create a post param expeditions={expeditionCode} joining with &
-        expedition.params <- paste0(expedition.names, "=", expeditions, collapse="&")
-        query.params <- paste0(query.params, expedition.params)
-    }
-
-    if (length(filters) > 0) {
-        # for each name in list create a post param name=value joining with &
-        filter.params <- paste0(names(filters), "=", unname(filters), collapse="&")
-
-        if (nchar(query.params) > 0) {
-            query.params <- paste0(query.params, "&", filter.params)
-        } else {
-            query.params <- paste0(query.params, filter.params)
-        }
-    }
+#' @export
+queryMetadata <- function(expeditions=list(), filters=list(), names=NULL) {
+    query.params <- prepareQueryParams(expeditions, filters)
 
     r <- POST(fimsQueryUrl,
             body=I(query.params),
@@ -100,4 +86,66 @@ query <- function(expeditions=list(), filters=list(), names=NULL) {
     }
 
     return(df)
+}
+
+prepareQueryParams <- function(expeditions, filters) {
+    query.params = ""
+
+    if (length(expeditions) > 0) {
+        expedition.names <- rep("expeditions", length(expeditions))
+        # for each expeditionCode in list create a post param expeditions={expeditionCode} joining with &
+        expedition.params <- paste0(expedition.names, "=", expeditions, collapse="&")
+        query.params <- paste0(query.params, expedition.params)
+    }
+
+    if (length(filters) > 0) {
+        # for each name in list create a post param name=value joining with &
+        filter.params <- paste0(names(filters), "=", unname(filters), collapse="&")
+
+        if (nchar(query.params) > 0) {
+            query.params <- paste0(query.params, "&", filter.params)
+        } else {
+            query.params <- paste0(query.params, filter.params)
+        }
+    }
+    return(query.params)
+}
+
+
+#' fetch Fasta sequences from the dipnet database
+#'
+#' @param marker      the marker to fetch
+#' @param expeditions list of expeditions to include in the query. The default is all expeditions
+#' @param filters     list of "column name":"value" pairs to include in the query. The special column name "_all" will do
+#'                    a full text search in all columns for the value
+#' @export
+queryFasta <- function(marker, expeditions=list(), filters=list()) {
+    query.params <- prepareQueryParams(expeditions, filters)
+
+    if (nchar(query.params) > 0) {
+        query.params <- paste0(query.params, "&", "fastaSequence.urn:marker=", marker)
+    } else {
+        query.params <- paste0(query.params, "fastaSequence.urn:marker=", marker)
+    }
+
+    r <- POST(fimsFastaQueryUrl,
+        body=I(query.params),
+        encode="form",
+        content_type("application/x-www-form-urlencoded")
+    )
+
+    stop_for_status(r)
+
+    temp <- tempfile()
+
+    writeBin(content(r, "raw"), temp)
+
+    unzip(temp, files="dipnet-fims-output.fasta")
+
+    if (file.info("dipnet-fims-output.fasta")$size == 0) {
+        print("no fasta sequences found")
+        return()
+    }
+
+    return(fasta2DNAbin("dipnet-fims-output.fasta"))
 }
--- README.md
+++ README.md
@@ -21,27 +21,32 @@ authenticate("user", "pass")
 The query function will execute a query against the fims database. The results are returned as a data.frame
 The most basic query will return all the samples in the database:
 ```
-df <- query()
+df <- queryMetadata()
 ```
 
 The following query will return all samples for the expeditions "TEST" and "TEST2"
 ```
-df <- query(expeditions=list("TEST", "TEST2))
+df <- queryMetadata(expeditions=list("TEST", "TEST2))
 ```
 
 The following query will the return "materialSampleID" and "bcid" columns for all samples.
 ```
-df <- query(names=list("materialSampleID", "bcid))
+df <- queryMetadata(names=list("materialSampleID", "bcid))
 ```
 
 The following query will the return "materialSampleID" and "bcid" columns for all samples where a full text search matches "Chordata"
 ```
-df <- query(names=list("materialSampleID", "bcid), filters=list("_all"="Chordata")
+df <- queryMetadata(names=list("materialSampleID", "bcid), filters=list("_all"="Chordata")
 ```
 
 The following query will the return "materialSampleID" and "bcid" columns for the samples in "TEST" expeditions where "yearCollected" = 2008
 ```
-df <- query(expeditions=list("TEST"), names=list("materialSampleID", "bcid), filters=list(yearCollected="2008")
+df <- queryMetadata(expeditions=list("TEST"), names=list("materialSampleID", "bcid), filters=list(yearCollected="2008")
+```
+
+The following will fetch "CO1" fasta sequences and return a DNABin 
+```
+fasta <- queryFasta("C01", expeditions=list("TEST"), filters=list(yearCollected="2008")
 ```
 
 You can fetch a list of expeditionCodes that are available to query:
